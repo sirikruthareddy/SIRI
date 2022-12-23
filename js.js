716cf4476dac9d2edce306fd46c234f04723764a
sirikruthareddy <121285889+sirikruthareddy@users.noreply.github.com>
2022-12-23 15:06:06 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+function sum(a,b){
+    let c=a+b;
+    console.log(c);
+}
+sum(5,10);
---
+++ object.js
@@ -0,0 +1,9 @@
+let obj1={
+    key1:"hello",
+    key2:"siri",
+    hello:function(){
+        console.log("hello world");
+    },
+};
+obj1.siri()
+obj1("hello")
